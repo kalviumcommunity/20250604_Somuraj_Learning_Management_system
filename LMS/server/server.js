98c428d0d43cd6cffd66b19dda7db2dc7675525e
somuraj-krishnamurthy <somurajkrishnamurthy@gmail.com>
2025-08-23 11:48:21 +0530

    second commit

--- LMS/server/server.js
+++ LMS/server/server.js
@@ -18,7 +18,7 @@ app.use(cors())
 app.get('/', (req,res) => res.send("API Working"))
 app.post('/clerk', express.json(), clerkWebhooks)
 
-//Port
+//Port Address
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`)
